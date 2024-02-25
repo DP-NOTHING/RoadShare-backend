5a5a8fe6ac0028093685b88c78233b2b793a92e7
Deep Patel <deep1907rp@gmail.com>
2024-02-25 17:30:42 +0530

    updated carApi

--- apis/CarApi.js
+++ apis/CarApi.js
@@ -6,15 +6,18 @@ module.exports = {
     /**
      * Endpoint for getting a list of available trims of a make,model, and year
      */
-    app.post("/api/findCar", async (req, res, next) => {
+     app.post("/api/findCar", async (req, res, next) => {
       if (req && req.body) {
+        console.log(req.body);
         // Get year,make, and model from the request
         let make = req.body["make"];
         let year = req.body["year"];
         let model = req.body["model"];
+        console.log(req.body);
 
         try {
           // Get the trim data for the requested car
+          console.log(`https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=${year}&make=${make}&model=${model}`)
           const resp = await axios.get(
             `https://www.fueleconomy.gov/ws/rest/vehicle/menu/options?year=${year}&make=${make}&model=${model}`,
             {
@@ -23,19 +26,30 @@ module.exports = {
               },
             }
           );
-          // ensure that the request was sucessful
+          console.log(resp.data);
+          // ensure that the request was successful
           if (resp && resp.data) {
             let vehicle_data = resp.data;
+            let id;
+            // Check if menuItem is an array
+            if (Array.isArray(vehicle_data.menuItem)) {
+              // Assuming you want the first item's value
+              id = vehicle_data.menuItem[0]["value"];
+            } else if (typeof vehicle_data.menuItem === 'object') {
+              // If menuItem is an object, directly extract value
+              id = vehicle_data.menuItem["value"];
+            } else {
+              // Handle other cases where menuItem is not as expected
+              throw new Error("Invalid menuItem format in response");
+            }
             res.status(200).json({
               make: make,
               year: year,
               model: model,
-              id: vehicle_data["menuItem"][0]["value"],
+              id: id,
             });
           } else {
-            res
-              .status(400)
-              .json({ error: "Bad Response From Fueleconomy Api" });
+            res.status(400).json({ error: "Bad Response From Fueleconomy Api" });
           }
         } catch (error) {
           res.status(404).json({ error: "Error from fueleconomy api" });
@@ -44,6 +58,7 @@ module.exports = {
         res.status(404).json({ error: "No Request Body specified" });
       }
     });
+
 
     /**
      * Endpoint for getting info about a certain car given an id
